5d285681c331c1622d36d150f8b4e080d31a8bb6
Kelvinluk827 <suiheiluk@gmail.com>
2021-03-19 04:01:35 +0800

    Inverted index, forward index, lookup table seemingly done without verification

--- src/main/java/hk/ust/cse/comp4321/proj1/Main.java
+++ src/main/java/hk/ust/cse/comp4321/proj1/Main.java
@@ -7,20 +7,55 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
 
-    public static void crawl(String crawlUrl, String forwardDb) {
+    public static void crawl(String crawlUrl, String forwardDb, String invertedDb, String lookupDb) {
         Crawler crawler = new Crawler(crawlUrl);
         crawler.crawlLoop();
         System.out.println("\nSuccessfully Returned");
 
-        // put in database
+        // put in databases
         List<DocumentRecord> documentRecordList = crawler.getDocumentRecords();
         try {
-            RocksStringMap<DocumentRecord> db = new RocksStringMap<>(forwardDb);
+            RocksIntegerMap<DocumentRecord> forwardDatabase = new RocksIntegerMap<>(forwardDb);
+            RocksStringMap<TreeMap<Integer, Integer>> invertedDatabase = new RocksStringMap<>(invertedDb);
+            RocksStringMap<Integer> urlDatabase = new RocksStringMap<>(lookupDb);
+
             for (DocumentRecord documentRecord : documentRecordList) {
-                db.put(documentRecord.getUrl().toString(), documentRecord);
+//                db.put(documentRecord.getUrl().toString(), documentRecord);
+                Integer urlKey = urlDatabase.get(documentRecord.getUrl().toString());
+                Integer currentKey;
+
+                if (urlKey == null){  // if url not exist in database
+                    // get a new key to for this url/documentRecord and update DB
+                    currentKey = forwardDatabase.getNextID();
+                    urlDatabase.put(documentRecord.getUrl().toString(), currentKey);
+
+                } else {
+                    currentKey = urlDatabase.get(documentRecord.getUrl().toString());
+
+                    DocumentRecord dbRecord = forwardDatabase.get(currentKey);
+                    if (dbRecord.getLastModificationDate().equals(documentRecord.getLastModificationDate())) {
+                        // same modification date, do nothing
+                        continue;
+                    }
+                }
+
+                // update forward index and inverted index
+                forwardDatabase.put(currentKey, documentRecord);
+                for (Map.Entry<String, Integer> item: documentRecord.getFreqTable().entrySet()){
+                    String keyword = item.getKey();
+                    TreeMap<Integer, Integer> data = invertedDatabase.get(keyword);
+                    // can be empty if keyword is not yet in the inverted index
+                    if (data == null){
+                        data = new TreeMap<>();
+                    }
+                    data.put(currentKey, item.getValue());
+                    invertedDatabase.put(keyword, data);
+                }
             }
 
         } catch (RocksDBException | IOException | ClassNotFoundException e) {
@@ -49,6 +84,10 @@ public class Main {
         String crawlUrl = "https://www.cse.ust.hk/";
         @CommandLine.Option(names = {"-f", "--forward-index"}, description = "The database name of the forward index")
         String forwardDb = "ForwardIndex";
+        @CommandLine.Option(names = {"-i", "--inverted-index"}, description = "The database name of the inverted index")
+        String invertedDb = "InvertedIndex";
+        @CommandLine.Option(names = {"-l", "--lookup-table"}, description = "The database name of the url to id lookup table")
+        String lookupDb = "LookupTable";
 
         @CommandLine.Option(names = {"-p", "--print"}, description = "Printing database forward index to file")
         boolean shouldPrintRecords = false;
@@ -70,7 +109,8 @@ public class Main {
         }
 
         if (commandOptions.shouldCrawl) {
-            crawl(commandOptions.crawlUrl, commandOptions.forwardDb);
+            crawl(commandOptions.crawlUrl, commandOptions.forwardDb, commandOptions.invertedDb,
+                    commandOptions.lookupDb);
         }
 
         if (commandOptions.shouldPrintRecords) {
