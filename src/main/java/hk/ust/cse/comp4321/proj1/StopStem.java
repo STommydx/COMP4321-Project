2618016a95fa0556b9f75247022499c17e0a8911
STommydx <ttoo74@gmail.com>
2021-03-29 22:58:00 +0800

    move stopword text to resource

--- src/main/java/hk/ust/cse/comp4321/proj1/StopStem.java
+++ src/main/java/hk/ust/cse/comp4321/proj1/StopStem.java
@@ -1,31 +1,37 @@
 package hk.ust.cse.comp4321.proj1;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.util.HashSet;
 
 public class StopStem {
-    private Porter porter;
-    private java.util.HashSet<String> stopWords;
+    private static final String STOPWORD_RESOURCE = "stopwords-en.txt";
 
-    public boolean isStopWord(String str) {
-        return stopWords.contains(str);
-    }
+    private final Porter porter;
+    private final HashSet<String> stopWords;
 
-    public StopStem(String str) {
-        super();
+    public StopStem() {
         porter = new Porter();
-        stopWords = new java.util.HashSet<String>();
+        stopWords = new HashSet<>();
         try {
-            BufferedReader reader = new BufferedReader(new FileReader(str));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stopWords.add(line);
+            InputStream is = getClass().getClassLoader().getResourceAsStream(STOPWORD_RESOURCE);
+            if (is != null) {
+                try (BufferedReader reader = new BufferedReader(new InputStreamReader(is))) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        stopWords.add(line);
+                    }
+                }
+                System.out.println("Total of " + stopWords.size() + " stopwords loaded.");
+            } else {
+                System.err.println("Warning: Fail to open resource file: " + STOPWORD_RESOURCE);
             }
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public boolean isStopWord(String str) {
+        return stopWords.contains(str);
     }
 
     public String stem(String str) {
