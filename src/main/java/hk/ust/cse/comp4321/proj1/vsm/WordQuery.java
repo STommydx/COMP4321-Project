b5dc85923838ac8ae198d5268e3e32bed749fe2a
STommydx <ttoo74@gmail.com>
2021-04-15 22:50:29 +0800

    Javadoc basics

--- src/main/java/hk/ust/cse/comp4321/proj1/vsm/WordQuery.java
+++ src/main/java/hk/ust/cse/comp4321/proj1/vsm/WordQuery.java
@@ -11,8 +11,22 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+/**
+ * The {@code WordQuery} class represents queries under the vector space model.
+ * <p>
+ * It is an abstract class which allows vector space model operations such as
+ * getting the representation of documents in vector space and calculating
+ * similarity scores for the query.
+ *
+ * @see Query
+ * @see SimpleWordQuery
+ * @see MultiWordQuery
+ */
 public abstract class WordQuery extends Query {
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public Map<Integer, Double> getSimilarityScore(ForwardIndex forwardIndex, InvertedIndex invertedIndex, Set<Integer> docs) throws RocksDBException, IOException, ClassNotFoundException {
         Map<Integer, Map<String, Double>> documentVectors = getDocumentVectors(forwardIndex, invertedIndex, docs);
@@ -26,8 +40,24 @@ public abstract class WordQuery extends Query {
         return similarityScores;
     }
 
+    /**
+     * Gets the query vector of the query.
+     *
+     * @return the query vector
+     */
     public abstract Map<String, Double> getQueryVector();
 
+    /**
+     * Retrieves the document vectors for a set of documents.
+     *
+     * @param forwardIndex  the forward index RocksDB
+     * @param invertedIndex the inverted index RocksDB
+     * @param docs          the set of documents to retrieve document vectors
+     * @return a map that maps the document index to the document vectors
+     * @throws RocksDBException       if there is an error from RocksDB
+     * @throws IOException            if there is an I/O problem
+     * @throws ClassNotFoundException if the deserialization fails
+     */
     public static Map<Integer, Map<String, Double>> getDocumentVectors(ForwardIndex forwardIndex, InvertedIndex invertedIndex, Set<Integer> docs) throws RocksDBException, IOException, ClassNotFoundException {
         Set<String> terms = new HashSet<>();
         Map<Integer, Map<String, Double>> termFrequencies = new HashMap<>();
