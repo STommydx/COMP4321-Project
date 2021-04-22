853eba74f396822576bf9e430d8a105122230c6a
yywong340 <janice.yyw@gmail.com>
2021-04-22 16:09:37 +0800

    JavaDoc - Testing

--- src/main/java/hk/ust/cse/comp4321/proj1/nlp/NLPUtils.java
+++ src/main/java/hk/ust/cse/comp4321/proj1/nlp/NLPUtils.java
@@ -3,19 +3,31 @@ package hk.ust.cse.comp4321.proj1.nlp;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+/**
+ * The {@code NLPUtils} class is an utility class
+ * that contains utility methods for assisting the stemming and stop words removal procedure.
+ */
 public class NLPUtils {
     private static final StopStem stopStem = new StopStem();
 
     private NLPUtils() {
     }
 
+    /**
+     * Checks if the string is non-null and only compose of alphabets and numbers
+     *
+     * @param s
+     * @return {@code true} if the string is non-null and only compose of alphabets or numbers followed by non-white-space characters
+     */
     public static boolean isAlphaNumeric(String s) {
         return s != null && s.matches("^[a-zA-Z0-9]+$");
     }
 
     /**
+     * Checks if the keyword is a stop word by utilizing method defined at StopStem.java
+     *
      * @param word
-     * @return
+     * @return {@code true} if the word is not a stop word
      */
     public static boolean stopwordFilter(String word) {
         if (word.startsWith("_")) return true;  // special tokens
@@ -23,8 +35,10 @@ public class NLPUtils {
     }
 
     /**
+     * Returns the stemmed word for an input by utilizing method defined at StopStem.java
+     *
      * @param word
-     * @return
+     * @return stemmed word
      */
     public static String stemFilter(String word) {
         if (word.startsWith("_")) return word;  // special tokens
--- src/main/java/hk/ust/cse/comp4321/proj1/nlp/StopStem.java
+++ src/main/java/hk/ust/cse/comp4321/proj1/nlp/StopStem.java
@@ -6,12 +6,23 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashSet;
 
+/**
+ * The {@code StopStem} class
+ *
+ *
+ */
 public class StopStem {
     private static final String STOPWORD_RESOURCE = "stopwords-en.txt";
 
     private final Porter porter;
     private final HashSet<String> stopWords;
 
+    /**
+     * Load all stop words from "stopwords-en.txt" to stopWords
+     * Report warning if failed to load the stop words
+     *
+     * @@throws IOException            if there is an I/O problem
+     */
     public StopStem() {
         porter = new Porter();
         stopWords = new HashSet<>();
@@ -33,10 +44,22 @@ public class StopStem {
         }
     }
 
+    /**
+     * Checks if the string is a stop word
+     *
+     * @param str
+     * @return {@code true} if the string is a stop word
+     */
     public boolean isStopWord(String str) {
         return stopWords.contains(str);
     }
 
+    /**
+     * Returns the stemmed string of an input by utilizing Porter.java
+     *
+     * @param str
+     * @return the stemmed string
+     */
     public String stem(String str) {
         return porter.stripAffixes(str);
     }
